5922c771deccd4210ab960fe1b58ff28233de7eb
Tomaž Erjavec <tomaz.erjavec@ijs.si>
2020-03-19 23:40:55 +0100

    Towards a vertical file.

---
+++ Orig/Scripts/joinvert.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use warnings;
+use utf8;
+$indexFile = shift;
+$inDir = shift;
+$outFile = shift;
+binmode(STDOUT,'utf8');
+binmode(STDERR,'utf8');
+
+#Read in author index and store in hash
+open IDX, '<:utf8', $indexFile or die "Can't open input $indexFile\n";
+my $Heading = <IDX>;
+while (<IDX>) {
+    chomp;
+    my ($id, $name, $viaf, $sex, $birth, $death, $title, 
+	$subtitle, $printed, $digitised, $period, $size, $canon, 
+	$words, $tokens) = split /\t/;
+    $printed{$printed} = $id;
+}
+close IDX;
+unlink $outFile;
+foreach $year (sort keys %printed) {
+    $inFile = "$inDir/" . $printed{$year} . "-L2.vert";
+    print STDERR "$year ";
+    `cat $inFile >> $outFile`;
+}
+print STDERR "\n";
