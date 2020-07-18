add22fa84e3dce884b72229e72164dfff1e35b6b
Tomaž Erjavec <tomaz.erjavec@ijs.si>
2020-07-18 18:20:30 +0200

    Fixed modernised.

--- Orig/Scripts/modernised.pl
+++ Orig/Scripts/modernised.pl
@@ -5,14 +5,14 @@ my @INFILES = glob(shift);
 binmode(STDOUT,'utf8');
 binmode(STDERR,'utf8');
 foreach $file (@INFILES) {
-    ($id)=$file=~m|([^/]+)\.txt|;
     open(TBL,$file);
     binmode(TBL,'utf8');
     $meta = 0;
     $words = 0;
     $mods = 0;
     while (<TBL>) {
-	if    (m|<bibl type="printSource">|) {$meta = 1}
+	if    (m|<TEI |) {($id) = m|xml:id="(.+?)"|}
+	elsif (m|<bibl type="printSource">|) {$meta = 1}
 	elsif (m|</bibl>|) {$meta = 0}
 	elsif ($meta and m|<author.*?>(.+)</author>|) {$author = $1}
 	elsif ($meta and m|<title.*?>(.+)</title>|) {$title = $1}
@@ -23,7 +23,9 @@ foreach $file (@INFILES) {
 	}
     }
     close TBL;
-    print join("\t", $id, $author, $title, $date, $words, $mods);
-    ...
-    print "\n"
+    printf "%d\t%d\t%d\t%0.2f\t%s\t%s\t%s\n", $date, $words, $mods, 100 * $mods / $words, 
+    $id, $author, $title;
+    $allwords += $words;
+    $allmods += $mods;
 }
+printf "*\t%d\t%d\t%0.2f\t*\t*\t*\n", $allwords, $allmods, 100 * $allmods / $allwords, 
